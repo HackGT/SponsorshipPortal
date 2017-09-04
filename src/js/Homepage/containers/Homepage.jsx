c44564e96eaa680369e8112299c4e5be9762dfd3
anish <anishvisaria98@gmail.com>
2017-09-04 17:26:13 -0400

    user table ui work

--- src/js/Homepage/containers/Homepage.jsx
+++ src/js/Homepage/containers/Homepage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Container, Form, Grid, Header, Input, Menu } from 'semantic-ui-react';
+import { Button, Container, Form, Grid, Header, Input, Menu, Table, Checkbox, Icon } from 'semantic-ui-react';
 
 class Homepage extends React.Component {
   render() {
@@ -15,6 +15,12 @@ class Homepage extends React.Component {
             />
           </Menu.Header>
           <Menu.Item
+            position="center"
+            name="pile"
+          >
+            <Button primary>Your Pile</Button>
+          </Menu.Item>
+          <Menu.Item
             position="right"
             name="logout"
           />
@@ -24,11 +30,68 @@ class Homepage extends React.Component {
             <Form>
               <Form.Field>
                 <Input
-                  label="Search for HackGT participant"
-                  placeholder="Search here"
+                  label="HackGT Participant Search"
+                  placeholder="Enter name or keywords"
                 />
               </Form.Field>
             </Form>
+          </Grid.Row>
+          <Grid.Row columns="1">
+            <Table compact celled definition>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell />
+                  <Table.HeaderCell>Name</Table.HeaderCell>
+                  <Table.HeaderCell>School</Table.HeaderCell>
+                  <Table.HeaderCell>E-mail address</Table.HeaderCell>
+                  <Table.HeaderCell>Resume</Table.HeaderCell>
+                  <Table.HeaderCell>Skills</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+
+              <Table.Body>
+                <Table.Row>
+                  <Table.Cell collapsing>
+                    <Checkbox slider />
+                  </Table.Cell>
+                  <Table.Cell>John Lilki</Table.Cell>
+                  <Table.Cell>Georgia Tech</Table.Cell>
+                  <Table.Cell>jhlilk22@yahoo.com</Table.Cell>
+                  <Table.Cell><Button>View Resume</Button></Table.Cell>
+                  <Table.Cell>Java, Python, C</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell collapsing>
+                    <Checkbox slider />
+                  </Table.Cell>
+                  <Table.Cell>Jamie Harington</Table.Cell>
+                  <Table.Cell>Harvard</Table.Cell>
+                  <Table.Cell>jamieharingonton@yahoo.com</Table.Cell>
+                  <Table.Cell><Button>View Resume</Button></Table.Cell>
+                  <Table.Cell>Go, Ruby, Haskell</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell collapsing>
+                    <Checkbox slider />
+                  </Table.Cell>
+                  <Table.Cell>Jill Lewis</Table.Cell>
+                  <Table.Cell>Princeton</Table.Cell>
+                  <Table.Cell>jilsewris22@yahoo.com</Table.Cell>
+                  <Table.Cell><Button>View Resume</Button></Table.Cell>
+                  <Table.Cell>SQL, Docker, Kubernetes</Table.Cell>
+                </Table.Row>
+              </Table.Body>
+
+              <Table.Footer fullWidth>
+                <Table.Row>
+                  <Table.HeaderCell />
+                  <Table.HeaderCell colSpan='4'>
+                    <Button size='small'>Add to Pile</Button>
+                    <Button size='small'>Add All to Pile</Button>
+                  </Table.HeaderCell>
+                </Table.Row>
+              </Table.Footer>
+            </Table>
           </Grid.Row>
         </Grid>
       </Container>
