de68f8f7459a12b5294d6caa3e9342c56581ccea
RSLi <robertsli@163.com>
2017-09-24 18:44:35 -0400

    Decorate HomePage

--- src/js/containers/pages/HomePage.jsx
+++ src/js/containers/pages/HomePage.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Container } from 'semantic-ui-react';
+import { Container, Step } from 'semantic-ui-react';
 
 class HomePage extends React.Component {
   render() {
+    // For sementics ui Step component
+    const steps = [
+      { icon: 'search', title: 'Search', description: 'Find the Talent you need' },
+      { icon: 'plus', title: 'Select', description: 'Add Candidates to Your Selection' },
+      { icon: 'cloud download', title: 'Export', description: 'Download Selected Data and Contacts' },
+    ];
+
     return (
-      <Container>
-        <p>This page is under construction</p>
+      <Container textAlign="center">
+        <h1>Welcome to HackGT Sponsorship Portal</h1>
+        <h3>Access, Manage, Recruit and Contact Talents</h3>
+        <Step.Group size="small" items={steps} />
       </Container>
     );
   }
