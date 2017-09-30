660b2c18d6e4e8a6f772ee0398e763587a271b46
RSLi <robertsli@163.com>
2017-09-30 13:13:47 -0400

    Add padding for Navbar

--- src/js/components/navigation/Navbar.jsx
+++ src/js/components/navigation/Navbar.jsx
@@ -1,34 +1,36 @@
 import React from 'react';
-import { Button, Header, Menu } from 'semantic-ui-react';
+import { Container, Button, Header, Menu } from 'semantic-ui-react';
 
 class Navbar extends React.Component {
   render() {
     const page = this.props.location.pathname;
     const push = this.props.push;
     return (
-      <Menu secondary>
-        <Menu.Header>
-          <Header
-            image="/src/assets/hackgt-logo.png"
-            content="Sponsorship Portal"
-            textAlign="center"
+      <Container>
+        <Menu secondary>
+          <Menu.Header>
+            <Header
+              image="/src/assets/hackgt-logo.png"
+              content="Sponsorship Portal"
+              textAlign="center"
+            />
+          </Menu.Header>
+          <Menu.Item
+            position="left"
+            name="pile"
+          >
+            <Button.Group>
+              <Button primary={page === '/'} onClick={() => push('/')}>Home</Button>
+              <Button primary={page === '/participant-profile'} onClick={() => push('/participant-profile')}>Workspace</Button>
+              <Button primary={page === '/export'} onClick={() => push('/export')}>Export</Button>
+            </Button.Group>
+          </Menu.Item>
+          <Menu.Item
+            position="right"
+            name="logout"
           />
-        </Menu.Header>
-        <Menu.Item
-          position="left"
-          name="pile"
-        >
-          <Button.Group>
-            <Button primary={page === '/'} onClick={() => push('/')}>Home</Button>
-            <Button primary={page === '/participant-profile'} onClick={() => push('/participant-profile')}>Workspace</Button>
-            <Button primary={page === '/export'} onClick={() => push('/export')}>Export</Button>
-          </Button.Group>
-        </Menu.Item>
-        <Menu.Item
-          position="right"
-          name="logout"
-        />
-      </Menu>
+        </Menu>
+      </Container>
     );
   }
 }
