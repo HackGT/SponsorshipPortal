5b323a3098e6907479417b7c03756a00f6b209c1
RSLi <robertsli@163.com>
2017-10-01 18:35:18 -0400

    Add GlobalLoader to global ui

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ConnectedNavbar from './containers/navigation/ConnectedNavbar';
+import GlobalLoader from './containers/global/GlobalLoader';
 
 export default function App({ children, location }) {
   return (
     <div>
       <ConnectedNavbar location={location} />
+      <GlobalLoader />
       <div style={{ marginTop: '1.5em' }}>{children}</div>
     </div>
   );
