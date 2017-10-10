76459fa8e61c5eff534c0b4fa0752560c0d33930
RSLi <robertsli@163.com>
2017-10-10 13:10:49 -0400

    Prevent users from accidentally exiting the page

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -8,13 +8,25 @@ import '../css/index.css';
 
 import render from './Routes';
 
+// initializae store and router
 const initialState = Map();
 const store = configureStore(initialState);
 const reactRouterReduxHistory = configureHistory(store);
 
+// Starts rendering
 render(store, reactRouterReduxHistory);
 
+/**
+ * Expose store globally
+ */
 export default store;
 
 // Particle Js Decorations
 window.particlesJS('particle', particlesOption);
+
+// Prevent users from accidentally leaving the page
+window.onbeforeunload = (e) => {
+  const dialogText = 'You are leaving the Sponsorship Portal. Are you sure?';
+  e.returnValue = dialogText;
+  return dialogText;
+};
