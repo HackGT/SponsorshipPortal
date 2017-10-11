326729fa6b5de2aa44d6a7a8a5c65091e45b4e28
RSLi <robertsli@163.com>
2017-10-11 10:42:15 -0400

    Fix #25

--- frontend/src/js/configs.js
+++ frontend/src/js/configs.js
@@ -1,2 +1,2 @@
-export const HOST = window.location.protocol + '//' + window.location.hostname;
+export const HOST = window.location.protocol + '//' + window.location.host;
 export const TITLE = 'HackGT Sponsorship Portal'; // TODO: This is not in use yet
