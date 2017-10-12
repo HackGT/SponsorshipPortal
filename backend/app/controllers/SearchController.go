955faecfbb053f0abe2d2f558cb8b2281bfa2228
anish <anishvisaria98@gmail.com>
2017-10-12 12:49:14 -0400

    metrics support

--- backend/app/controllers/SearchController.go
+++ backend/app/controllers/SearchController.go
@@ -1,8 +1,12 @@
 package controllers
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/HackGT/SponsorshipPortal/backend/app"
 	"github.com/blevesearch/bleve"
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/revel/revel"
 )
 
@@ -16,7 +20,7 @@ func (c Search) Index() revel.Result {
 	phrase := jsonData["query"].(string)
 	tokenStr := jsonData["token"].(string)
 	res := make(map[string]interface{})
-	_, valid := ParseToken(tokenStr)
+	token, valid := ParseToken(tokenStr)
 	if !valid {
 		res["error"] = "invalid token"
 		return c.RenderJSON(res)
@@ -24,8 +28,29 @@ func (c Search) Index() revel.Result {
 	query := bleve.NewQueryStringQuery(phrase)
 	search := bleve.NewSearchRequest(query)
 	searchResults, err := app.BleveIndex.Search(search)
-	//originalDocs := getOriginalDocsFromSearchResults(searchResults, BleveIndex)
 	resp := make(map[string]interface{})
+	if err == nil {
+		sponsors := app.PortalDB.Use("Sponsors")
+		claims := token.Claims.(jwt.MapClaims)
+		id, err := strconv.Atoi(claims["id"].(string))
+		if err != nil {
+			return c.RenderError(err)
+		}
+		readBack, err := sponsors.Read(id)
+		if err != nil {
+			return c.RenderError(err)
+		}
+		resultUUID := ""
+		for _, element := range searchResults.Hits {
+			resultUUID += element.ID
+		}
+		fmt.Println(fmt.Sprintf(`{ 
+			hackgtmetricsversion: 1,
+			serviceName: "sponsorshipportal-hackgt4-search",
+			values: {value:1},
+			tags: {search: "%s", user: "%s", resultNum: %d, resultUuid: "%s"}
+		`, phrase, readBack["company"], searchResults.Total, resultUUID))
+	}
 	resp["error"] = err
 	resp["results"] = searchResults
 	return c.RenderJSON(resp)
