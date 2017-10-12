2424fa9ce6c615017c806c4bab42b9779640fd34
RSLi <robertsli@163.com>
2017-10-12 12:39:42 -0400

    Make the step group responsive on smaller screens

--- frontend/src/js/containers/pages/HomePage.jsx
+++ frontend/src/js/containers/pages/HomePage.jsx
@@ -20,7 +20,7 @@ class HomePage extends React.Component {
         <Container textAlign="center" style={{ marginBottom: '40px' }}>
           <h1>{TITLE}</h1>
           {/* <h3>Access, Manage, Recruit and Contact Talents</h3> */}
-          <Step.Group size="small" items={steps} />
+          <Step.Group size="small" stackable="tablet" items={steps} />
         </Container>
         <ParticipantProfilePage />
         <ExportPage />
