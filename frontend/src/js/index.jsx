eecaea3bd233ce90227bc4d357eb8b8ad86c7abf
RSLi <robertsli@163.com>
2017-10-12 12:16:34 -0400

    Remove particle decoration effects

--- frontend/src/js/index.jsx
+++ frontend/src/js/index.jsx
@@ -2,7 +2,7 @@ import 'semantic-ui-css/semantic.min.css';
 import 'whatwg-fetch';
 import { Map } from 'immutable';
 import 'particles.js';
-import particlesOption from '../assets/particlesOption';
+// import particlesOption from '../assets/particlesOption';
 import { configureHistory, configureStore } from './configureStoreAndHistory';
 import '../css/index.css';
 import { logInWithToken } from './actions/auth';
@@ -22,8 +22,8 @@ render(store, reactRouterReduxHistory);
  */
 export default store;
 
-// Particle Js Decorations
-window.particlesJS('particle', particlesOption);
+// Particle Js Decorations (Disabled)
+// window.particlesJS('particle', particlesOption);
 
 // Log the user in when token is present in localStorage
 if (window.localStorage.getItem('token')) {
