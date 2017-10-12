3e581dd69740b3da04601370f920cb82e482029a
RSLi <robertsli@163.com>
2017-10-12 17:07:50 -0400

    Add logout button

--- frontend/src/js/containers/navigation/ConnectedNavbar.jsx
+++ frontend/src/js/containers/navigation/ConnectedNavbar.jsx
@@ -1,12 +1,13 @@
 import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
-
+import { logOut } from '../../actions/auth';
 import Navbar from '../../components/navigation/Navbar';
 
 function mapStateToProps(state, ownProps) {
   return {
     location: ownProps.location,
     ui: state.get('ui'),
+    auth: state.get('auth'),
   };
 }
 
@@ -14,6 +15,9 @@ function mapDispatchToProps(dispatch) {
   return {
     push: (uri) => {
       dispatch(push(uri));
+    },
+    logOut: () => {
+      dispatch(logOut());
     },
   };
 }
